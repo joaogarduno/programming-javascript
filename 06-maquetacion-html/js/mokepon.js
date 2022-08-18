2085b33b894e545aefc6949e9cce2eb339b94a1e
Joao Garduño <joao.garduno@gmail.com>
2022-08-18 13:21:29 -0500

    Estructura 11: Seleccion de mascota aleatoria del enemigo

--- 06-maquetacion-html/js/mokepon.js
+++ 06-maquetacion-html/js/mokepon.js
@@ -26,7 +26,36 @@ function selectPetsPlayer(){
     } else{
         alert('¡NO seleccionaste ninguna mascota!');
     }
+
+    // Llamando a la funcion de seleccion de mascota del enemigo despues de seleccionar la mascota del jugador
+    selectPetsEnemy();
 }
+
+// Funcion de la seleccion de mascota del enemigo
+function selectPetsEnemy(){
+    // guardando el numero aleatorio
+    let randomAttack = aleatorio(1, 3);
+
+    // variable para guardar el valor de la mascota, es decir la mascota elegida por la PC
+    let spanPetEnemy = document.querySelector('#petEnemy');
+
+     // Lugar o accion donde podamos decirle a nuestro codigo que nos diga cual es la variable o mascota que selecciona la PC
+    if(randomAttack == 1){
+        spanPetEnemy.innerHTML = '<strong>Hipodoge</strong>'
+    } else if(randomAttack == 2){
+        spanPetEnemy.innerHTML = '<strong>Capipepo</strong>';
+    } else{
+        spanPetEnemy.innerHTML = '<strong>Ratigueya</strong>';
+    }
+
+   
+}
+
+// Funcion para dar un valor aleatorio en la seleccion de mascota del enemigo o de la PC
+function aleatorio(min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
 
 // Metodo para nuestro navegador al cargar el HTML
 // Apenas carge nuestro HTML carge el juego
